1bb91d2fe1a967e7432fdcb5cbb731d140b00970
ViviZa <vivian.zaddach@gmail.com>
2020-01-22 11:13:04 +0100

    implement consumer

--- src/main/java/Consumer.java
+++ src/main/java/Consumer.java
@@ -1,2 +1,36 @@
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
 public class Consumer {
+
+    public void processMessages() {
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+        Connection connection = null;
+        try {
+            connection = connectionFactory.createConnection();
+            connection.start();
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Destination destination = session.createQueue("myQueue");
+            MessageConsumer consumer = session.createConsumer(destination);
+
+            Message message = consumer.receive(1000);
+
+            if (message instanceof TextMessage) {
+                TextMessage textMessage = (TextMessage) message;
+                String text = textMessage.getText();
+                System.out.println("Received: " + text);
+            } else {
+                System.out.println("Received: " + message);
+            }
+
+            /*consumer.close();
+            session.close();
+            connection.close();*/
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
